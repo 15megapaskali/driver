41e2a87557fb6847517f454dddaaa58b98916287
15MPa <m.pajda@gmail.com>
2020-08-14 12:06:30 +0200

    config scss and other updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,27 @@
 import React, {Component,useState,useEffect,} from "react";
 
 const Header = ()=>{
-    return null;
+    return (
+    <div className="header">
+        <div className="head-up">
+            <img src="/" alt="logo"/>
+            <img src="/" alt="ad"/>
+            <div className="login">
+                
+            </div>
+        </div>
+        <div className="head-down">
+            <div className="head-buttons">
+                <button>Porady</button>
+                <button>Forum</button>
+                <button>Quiz</button>
+            </div>
+        </div>
+
+
+
+
+    </div>);
 }
 
 export default Header;
