40fc0c284fe35c2b7a697c87b49d54ddfafbe783
15megapaskali <m.pajda@gmail.com>
2020-11-01 22:27:45 +0100

    layout mobile first

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,24 @@
 import React, {Component,useState,useEffect,} from "react";
 
+import ad from '../assets/u7.png';
+
 const Header = ()=>{
     return (
     <div className="header">
         <div className="head-up">
-            <img src="/" alt="logo"/>
-            <img src="/" alt="ad"/>
+            <div className="logo">
+                {/* <img src="/" alt="logo"/> */}
+                <span>drv</span>
+            </div>
+            
+            <div className="adver-bg">
+                <span>Patronat:</span>
+                <img src={ad} alt="ad" id="ad"/>
+            </div>
+            
             <div className="login">
-                
+                <span>user login</span>
+                <span>72 punkty | wyloguj</span>
             </div>
         </div>
         <div className="head-down">
